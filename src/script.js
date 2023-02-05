4c3a164ecaf767f18146538f7e54d2070d5ade75
Sqmantha0 <122523681+Sqmantha0@users.noreply.github.com>
2023-02-05 18:17:02 +0200

    JS updates

--- src/script.js
+++ src/script.js
@@ -18,13 +18,40 @@ dateNtime.innerHTML = `${day} ${hour}:${minutes}`;
 
 //
 
+function displayForecast(response) {
+    let forecastElement = document.querySelector(`.forecast`)
+
+    let forecastHTML = ``
+    let days = ["Thu", "Fri", "Sat"]
+    days.forEach(function(day) {
+        forecastHTML = forecastHTML + `
+        <div class="col day1">
+        <span class="forDay">${day}<div>
+        <img src="" class="" id="foricon1" alt=""/></div>
+        <div id="forTemp">18</div></span>
+        </div>`
+        
+        forecastElement.innerHTML = forecastHTML
+    })
+}
+
+function getForecast(coords) { 
+    console.log(coords) 
+    
+    let apiKey = "3dce9b1c66837262a25b3f448d354a76";
+    let apiUrl = 
+    `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`
+    console.log(apiUrl)
+    axios.get(apiUrl).then(displayForecast)
+}
+
 function displayTemperature(response) {
     let temp = document.querySelector(`.degrees`)
     let cityElement = document.querySelector(`.cityName`)
     let weather = document.querySelector(`.weather`)
     let humidity = document.querySelector(`.humidity`)
     let wind = document.querySelector(`.wind`)
-    let iconElement = document.querySelector("#icon")
+    let iconElement1 = document.querySelector("#icon")
     
     celciusTemp = response.data.main.temp
 
@@ -33,8 +60,12 @@ function displayTemperature(response) {
     weather.innerHTML = response.data.weather[0].description
     humidity.innerHTML = response.data.main.humidity + `%`
     wind.innerHTML = Math.round(response.data.wind.speed) + ` km/h`
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) 
-    iconElement.setAttribute("alt", response.data.weather[0].description)
+    iconElement1.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) 
+    iconElement1.setAttribute("alt", response.data.weather[0].description)
+
+    console.log(response)
+
+    getForecast(response.data.coord)
 }
 
 function searchCity(city) {
@@ -75,3 +106,5 @@ fahrenheit.addEventListener("click", displayFahrenheit)
 
 let celsius = document.querySelector("#celsius")
 celsius.addEventListener("click", displayCelsius)
+
+searchCity(`Lisbon`)
